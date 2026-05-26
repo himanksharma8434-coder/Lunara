10e636b7c1222be7565eb07a5222da9a52c31b97
Himank <himanksharma8434@gmail.com>
2026-05-26 12:00:00 +0530

    feat(models): add avatarUrl to UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -4,6 +4,7 @@ class UserModel {
   final String? phone;
   final String name;
   final int cycleLength;
+  final String? avatarUrl;
 
   UserModel({
     required this.uid,
@@ -11,6 +12,7 @@ class UserModel {
     this.phone,
     this.name = '',
     this.cycleLength = 28,
+    this.avatarUrl,
   });
 
   /// Convert to Map to send to Supabase
@@ -21,6 +23,7 @@ class UserModel {
       'phone': phone ?? '',
       'name': name,
       'cycle_length': cycleLength,
+      'avatar_url': avatarUrl,
       'created_at': DateTime.now().toIso8601String(),
     };
   }
@@ -33,6 +36,7 @@ class UserModel {
       phone: map['phone'],
       name: map['name'] ?? '',
       cycleLength: map['cycle_length'] ?? 28,
+      avatarUrl: map['avatar_url'],
     );
   }
 }
